64235049e5ba56dd1a8a4f98e233a11f80fde75f
Husam Burhan <husam.burhan@gmail.com>
2022-01-30 23:14:31 +0300

    modifications to several files to support shopping cart

--- chapter06/js/main.js
+++ chapter06/js/main.js
@@ -1,16 +1,78 @@
-let buttons = document.getElementsByName('addToCart');
+export function saveItem(item) {
+    let key = Object.keys(localStorage)
+                .find(e => e === String(item.id));
 
-for(let button of buttons){
-    button.addEventListener('click', (e)=>{
-    addToCart(e.target.attributes['id'].value);
-    });
+    if(key){
+        let tmp = JSON.parse(localStorage.getItem(key));
+        tmp.push(item);
+        localStorage.setItem(item.id, JSON.stringify(tmp));
+    }
+    else{
+        localStorage.setItem(item.id, JSON.stringify([item]));
+    }
 }
 
-function addToCart(id){
-    let current_number = Number(document.getElementsByClassName('counter')[0].innerHTML) || 0;
-    current_number++;      
-    
-    let counterElement = document.getElementsByClassName('counter')[0];
-    counterElement.innerHTML = current_number;
-    counterElement.classList.remove('zero');
+export function getItem(id) {
+    return window.localStorage.getItem(item.id);
 }
+
+export function getAllItems(){
+    const keys = Object.keys(window.localStorage);
+    let entries = [];
+
+    for(let key of keys){
+        entries.push(JSON.parse(localStorage.getItem(key)))
+    }
+
+    return entries;
+}
+
+export function updateShoppingCartList(){
+    let shoppingCartContent = document.getElementById('shoppingCartContent');
+    let counter = 1;
+    let total_products = 0;
+    let amount_due = 0;
+
+    for (let group of getAllItems()) {
+        const div = document.createElement('div');
+
+        div.className = 'row shopping-cart-item';
+
+        div.innerHTML = `
+            <div class="col-1">
+                <span>${counter}</span>
+            </div>
+            <div class="col-6">
+                <span>${group[0].course_name}</span>
+            </div>
+            <div class="col-1">
+                <span>${group.length}</span>
+            </div>
+            <div class="col-1">
+                <span>$${group[0].price}</span>    
+            </div>
+            <div class="col-1">
+                <span>$${group.length * group[0].price}</span>    
+            </div>
+            `;
+
+        shoppingCartContent.appendChild(div);
+        
+        amount_due += group.length * group[0].price;
+        counter++;
+        total_products += group.length;
+    }
+
+    document.getElementById('amountDue').innerHTML = '$' + amount_due;
+}
+
+export function updateCartLabel(){
+    const counterElement = document.getElementsByClassName('counter')[0];
+
+    if(window.localStorage.length > 0){
+        counterElement.innerHTML = window.localStorage.length; // items_in_cart_counter;
+        counterElement.classList.remove('zero');
+    }
+}
+
+
